6495d6516642edc61affe49543524d053c216978
mihailkochetkov94 <km11-94@mail.ru>
2022-11-14 00:23:16 +0400

    app

--- m5_quiz_resources/app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ m5_quiz_resources/app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.quiz_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.quiz_app.ui.main.MainFragment
@@ -7,7 +8,6 @@ import com.example.quiz_app.ui.main.MainFragment
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_Quiz_app)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
---
+++ m5_quiz_resources/app/src/main/java/com/example/quiz_app/TestSplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.quiz_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class TestSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+    }
+}
