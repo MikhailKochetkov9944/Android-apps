82cbe3d240cceb96a9228c9cb13a8d44d6937299
mihailkochetkov94 <km11-94@mail.ru>
2022-11-14 20:53:49 +0400

    app

--- m5_quiz_resources/app/src/main/java/com/example/quiz_app/SplashActivity.kt
+++ m5_quiz_resources/app/src/main/java/com/example/quiz_app/SplashActivity.kt
@@ -3,10 +3,16 @@ package com.example.quiz_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed(::startMainActivity, 3000)
+
+    }
+    private fun startMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
